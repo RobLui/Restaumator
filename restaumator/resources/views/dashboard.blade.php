c43c2afbd0b3dfa927363f9efdbd28a8b25d0a7b
Robbert <robbertluit@hotmail.com>
2017-11-27 15:44:54 +0100

    add basic dash

---
+++ restaumator/resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="black-bg">
+        @include('partials._nav')
+    </div>
+
+@endsection
--- restaumator/routes/web.php
+++ restaumator/routes/web.php
@@ -5,3 +5,5 @@ Route::get('/', 'ProductpageController@index');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/dashboard', 'DashboardController@index')->name('home');
