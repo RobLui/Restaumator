174d0325e3c05fe917d86610fab3eb2cf5e3ce36
Robbert <robbertluit@hotmail.com>
2017-11-27 16:04:34 +0100

    add dashboard & statistics view

--- restaumator/routes/web.php
+++ restaumator/routes/web.php
@@ -1,9 +1,19 @@
 <?php
 
-Route::get('/', 'ProductpageController@index');
+Route::get('/', 'ProductpageController@index')
+    ->name('productpage')
+;
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')
+    ->name('home')
+;
 
-Route::get('/dashboard', 'DashboardController@index')->name('home');
+Route::get('/dashboard', 'DashboardController@index')
+    ->name('dashboard')
+;
+
+Route::get('/statistics', 'StatisticsController@index')
+    ->name('statics')
+;
