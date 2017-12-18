bd9c63b80966b99f136a50244b10eb1bc6cd94cb
Robbert <robbertluit@hotmail.com>
2017-12-18 15:24:27 +0100

    add updated register

--- restaumator/resources/views/auth/login.blade.php
+++ restaumator/resources/views/auth/login.blade.php
@@ -3,43 +3,41 @@
 
 <div class="container text-center">
     <div class="margin-top-6 text-center"></div>
-    <div class="row text-center">
-        <div class="col-md-12 text-center">
+        <div class="row text-center">
+            <div class="col-md-12 text-center">
             <div class="panel panel-default text-center">
-                <div class="panel-heading text-center"><h2>Login</h2></div>
+                <div class="panel-heading text-center"><h2 class="margin-bottom-3">Login</h2></div>
 
                 <div class="panel-body text-center">
                     <form class="col-md-12 text-center" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
+                            <div class="form-group text-center {{ $errors->has('email') ? ' has-error' : '' }}">
+                                <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
-                        <div class="form-group text-center {{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                                <div class="col-md-6 col-md-offset-3 margin-center">
+                                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
 
-                            <div class="col-md-6 col-md-offset-3 margin-center">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+                                    @if ($errors->has('email'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                <label for="password" class="col-md-4 control-label">Password</label>
 
-                            <div class="col-md-6 col-md-offset-3  margin-center">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                                <div class="col-md-6 col-md-offset-3 margin-center">
+                                    <input id="password" type="password" class="form-control" name="password" required>
 
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
+                                    @if ($errors->has('password'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('password') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
                             </div>
-                        </div>
-
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4 margin-center">
                                 <div class="checkbox">
--- restaumator/resources/views/auth/register.blade.php
+++ restaumator/resources/views/auth/register.blade.php
@@ -1,21 +1,20 @@
 @extends('layouts.app')
 @include('partials._nav')
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="margin-top-6"></div>
-                <div class="panel-heading">Register</div>
+<div class="container text-center">
+    <div class="margin-top-6 text-center"></div>
+    <div class="row text-center">
+        <div class="col-md-12 text-center">
+            <div class="panel panel-default text-center">
+                <div class="panel-heading text-center"><h2 class="margin-bottom-3">Register</h2></div>
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+                <div class="panel-body text-center">
+                    <form class="col-md-12 text-center" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                        <div class="form-group text-center {{ $errors->has('name') ? ' has-error' : '' }}">
                             <label for="name" class="col-md-4 control-label">Name</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-6 col-md-offset-3 margin-center">
                                 <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
 
                                 @if ($errors->has('name'))
@@ -29,7 +28,7 @@
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-6 col-md-offset-3 margin-center">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
@@ -43,7 +42,7 @@
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                             <label for="password" class="col-md-4 control-label">Password</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-6 col-md-offset-3 margin-center">
                                 <input id="password" type="password" class="form-control" name="password" required>
 
                                 @if ($errors->has('password'))
@@ -57,14 +56,14 @@
                         <div class="form-group">
                             <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-6 col-md-offset-3 margin-center">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="col-md-6 col-md-offset-4 margin-center">
+                                <button type="submit" class="btn btn-primary loginbutton">
                                     Register
                                 </button>
                             </div>
@@ -75,3 +74,4 @@
         </div>
     </div>
 </div>
+
