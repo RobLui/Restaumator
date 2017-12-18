82290953d86789f4ccd6f35107ba27bf5d5926e8
Robbert <robbertluit@hotmail.com>
2017-12-18 13:04:03 +0100

    update login centering

--- restaumator/resources/views/auth/login.blade.php
+++ restaumator/resources/views/auth/login.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.app')
 @include('partials._nav')
 
-<div class="container">
-    <div class="margin-top-6"></div>
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+<div class="container text-center">
+    <div class="margin-top-6 text-center"></div>
+    <div class="row text-center">
+        <div class="col-md-12 text-center">
+            <div class="panel panel-default text-center">
+                <div class="panel-heading text-center"><h2>Login</h2></div>
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                <div class="panel-body text-center">
+                    <form class="col-md-12 text-center" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                        <div class="form-group text-center {{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-6 col-md-offset-3 margin-center">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
@@ -29,7 +29,7 @@
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                             <label for="password" class="col-md-4 control-label">Password</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-6 col-md-offset-3  margin-center">
                                 <input id="password" type="password" class="form-control" name="password" required>
 
                                 @if ($errors->has('password'))
@@ -41,7 +41,7 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
+                            <div class="col-md-6 col-md-offset-4 margin-center">
                                 <div class="checkbox">
                                     <label>
                                         <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
@@ -51,7 +51,7 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
+                            <div class="col-md-8 col-md-offset-4 margin-center">
                                 <button type="submit" class="btn btn-primary loginbutton">
                                     Login
                                 </button>
